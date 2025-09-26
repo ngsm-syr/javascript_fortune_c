5388b9d31c794b710670f3b5932b54cf0efc9068
Nagashima Sayuri <106061832+ngsm-syr@users.noreply.github.com>
2025-09-26 09:37:25 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -10,17 +10,24 @@ drawButton.addEventListener('click', () => {
 
     console.log(randomNumber); // ランダムな数字をコンソールに表示する（確認用）
 
+    // 既存のクラスを削除
+    resultDisplay.className = '';
+    document.body.className = '';
+
     // 0.3 未満だったら
     if (randomNumber < 0.3) {
         resultDisplay.textContent = '大吉';
+        document.body.classList.add('daikichi');
 
     // 0.8 未満だったら
     } else if (randomNumber < 0.8) {
         resultDisplay.textContent = '中吉';
+        document.body.classList.add('chukichi');
 
     // どちらでもない場合
     } else {
-        resultDisplay.textContent = '凶'; //
+        resultDisplay.textContent = '吉';
+        document.body.classList.add('kichi');
     }
 
 });
